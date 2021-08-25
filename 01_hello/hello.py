bc0283f7f6ac290c86df57b17bddaaae6cc4cb5a
Franco <herjaxx@yahoo.co.uk>
2021-08-25 14:01:15 +0900

    Refactored hello.py to put argparse stuff into a function.

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -4,15 +4,15 @@
 import argparse
 
 
-def main():
+def get_args():
     parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n',
-                        '--name',
-                        metavar='name',
-                        default='World',
-                        help='Name to greet')
-    args = parser.parse_args()
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
+    return parser.parse_args()
 
+
+def main():
+    args = get_args()
     print(f"Hello, {args.name}!")
 
 
