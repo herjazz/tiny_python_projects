db5f33baacbc666256f33958e0f100316a7bd674
Franco <herjaxx@yahoo.co.uk>
2021-08-31 21:17:51 +0900

    APPLES_REGEX: Made a version using regexes.

---
+++ 08_apples_and_bananas/apples_regex.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+Author : wrjt <wrjt@localhost>
+Date   : 2021-08-31
+Purpose: A program which substitutes vowels in text with a supplied vowel
+(default: 'a')
+"""
+
+import argparse
+import re
+import os
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Apples and bananas',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('text', metavar='text', help='Input text or file')
+
+    parser.add_argument('-v',
+                        '--vowel',
+                        help='The vowel to substitute',
+                        metavar='vowel',
+                        type=str,
+                        choices=list('aeiou'),
+                        default='a')
+
+    args = parser.parse_args()
+
+    if os.path.isfile(args.text):
+        with open(args.text, 'rt', encoding='utf-8') as stream:
+            args.text = stream.read().rstrip()
+
+    return args
+
+
+# --------------------------------------------------
+def main():
+    """ Main prog """
+
+    args = get_args()
+    text = args.text
+    vowel = args.vowel
+
+    text = re.sub('[aeiou]', vowel, text)
+    text = re.sub('[AEIOU]', vowel.upper(), text)
+
+    print(text)
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
