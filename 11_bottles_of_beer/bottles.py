6b2548664e6cf7314f22766b7aa80a25bf9d3495
Franco <herjaxx@yahoo.co.uk>
2021-09-03 20:37:43 +0900

    BOTTLES: Slimlined verse function and added unit test for it.

--- 11_bottles_of_beer/bottles.py
+++ 11_bottles_of_beer/bottles.py
@@ -7,7 +7,7 @@ Purpose: To generate "bottle of beer" song dpeneding on the value of n
 """
 
 import argparse
-import textwrap as tw
+# import textwrap as tw
 
 
 # --------------------------------------------------
@@ -35,30 +35,42 @@ def get_args():
 
 
 # --------------------------------------------------
-def verse(bottle: int) -> str:
-    """ Sing a verse """
-    if bottle == 2:
-        verse = tw.dedent("""\
-        2 bottles of beer on the wall,
-        2 bottles of beer,
-        Take one down, pass it around,
-        1 bottle of beer on the wall!
-        """)
-    elif bottle == 1:
-        verse = tw.dedent("""\
-        1 bottle of beer on the wall,
-        1 bottle of beer,
-        Take one down, pass it around,
-        No more bottles of beer on the wall!""")
-    else:
-        verse = tw.dedent(f"""\
-        {bottle} bottles of beer on the wall,
-        {bottle} bottles of beer,
-        Take one down, pass it around,
-        {bottle - 1} bottles of beer on the wall!
-        """)
+# def verse(bottle: int) -> str:
+#     """ Sing a verse """
+#     if bottle == 2:
+#         verse = tw.dedent("""\
+#         2 bottles of beer on the wall,
+#         2 bottles of beer,
+#         Take one down, pass it around,
+#         1 bottle of beer on the wall!
+#         """)
+#     elif bottle == 1:
+#         verse = tw.dedent("""\
+#         1 bottle of beer on the wall,
+#         1 bottle of beer,
+#         Take one down, pass it around,
+#         No more bottles of beer on the wall!""")
+#     else:
+#         verse = tw.dedent(f"""\
+#         {bottle} bottles of beer on the wall,
+#         {bottle} bottles of beer,
+#         Take one down, pass it around,
+#         {bottle - 1} bottles of beer on the wall!
+#         """)
 
-    return verse
+#     return verse
+
+
+def verse(bottle):
+    """ Sing a verse """
+
+    current_bottle = '1 bottle' if bottle == 1 else f'{bottle} bottles'
+    next_bottle = 'No more bottles' if bottle == 1 else f'{bottle - 1} bottle' if bottle == 2 else f'{bottle - 1} bottles'
+
+    return '\n'.join([
+        f'{current_bottle} of beer on the wall,', f'{current_bottle} of beer,',
+        'Take one down, pass it around,', f'{next_bottle} of beer on the wall!'
+    ])
 
 
 # --------------------------------------------------
@@ -66,10 +78,28 @@ def main():
     """ Main Prog """
 
     args = get_args()
-    num_bottles = args.num
 
-    for i in range(num_bottles, 0, -1):
-        print(verse(i))
+    # for i in range(args.num, 0, -1):
+    #     print(verse(i))
+
+    print('\n\n'.join(map(verse, range(args.num, 0, -1))))
+
+
+def test_verse():
+    """ Test verse """
+
+    last_verse = verse(1)
+    assert last_verse == '\n'.join([
+        '1 bottle of beer on the wall,', '1 bottle of beer,',
+        'Take one down, pass it around,',
+        'No more bottles of beer on the wall!'
+    ])
+
+    two_bottles = verse(2)
+    assert two_bottles == '\n'.join([
+        '2 bottles of beer on the wall,', '2 bottles of beer,',
+        'Take one down, pass it around,', '1 bottle of beer on the wall!'
+    ])
 
 
 # --------------------------------------------------
